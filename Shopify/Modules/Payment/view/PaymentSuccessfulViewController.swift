e8a9583c76efcfb142e559023f47b580db9485b1
aya <https://github.com/ayamashalyy>
2024-06-04 20:48:33 +0300

    add payment success screen

---
+++ Shopify/Modules/Payment/view/PaymentSuccessfulViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PaymentSuccessfulViewController.swift
+//  Shopify
+//
+//  Created by aya on 04/06/2024.
+//
+
+import UIKit
+
+class PaymentSuccessfulViewController: UIViewController {
+    @IBOutlet weak var backoHome: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupbackoHomeButton()
+        
+    }
+   
+    func setupbackoHomeButton() {
+        backoHome.backgroundColor = UIColor(hex: "#FF7D29")
+        backoHome.setTitleColor(UIColor.white, for: .normal)
+        backoHome.layer.cornerRadius = 10
+        backoHome.clipsToBounds = true
+    }
+    
+    @IBAction func backoHome(_ sender: UIButton) {
+        print("back home")
+    }
+
+}
