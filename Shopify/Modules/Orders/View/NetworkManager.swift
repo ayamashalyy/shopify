94692e4787e757dd0c0d3723390cc421cb95ead7
mayar <miarsaleh@gmail.com>
2024-06-07 14:12:33 -0700

    updateNetwork

--- Shopify/Modules/Orders/View/NetworkManager.swift
+++ Shopify/Modules/Orders/View/NetworkManager.swift
@@ -9,37 +9,39 @@ import Foundation
 import Alamofire 
 
 enum Endpoint: String {
-    case smartCollections = "smart_collections"
+    case smartCollections = "smart_collections.json"
     
 }
 
-
+enum Root: String {
+    case smartCollectionsRoot = "smart_collections"
+    
+}
 
 class NetworkManager {
     
-    static func fetchDataFromApi<T: Codable>(endpoint: Endpoint, objectType: T.Type, completion: @escaping (T?, Error?) -> Void) {
-        guard let url = URL(string: "https://" + API_KEY + ":" + TOKEN + baseUrl + endpoint.rawValue + ".json") else {
-            completion(nil, NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
-            return
-        }
+    static func fetchDataFromApi(endpoint: Endpoint, rootOfJson: Root, completion: @escaping (Data?, Error?) -> Void) {
+            guard let url = URL(string: "https://" + API_KEY + ":" + TOKEN + baseUrl + endpoint.rawValue) else {
+                completion(nil, NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
+                return
+            }
 
-        Alamofire.request(url).responseJSON { response in
-            switch response.result {
-            case .success(let value):
-                guard let json = value as? [String: Any], let smartCollections = json["smart_collections"] as? [[String: Any]] else {
-                    completion(nil, NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid JSON format"]))
-                    return
-                }
-                do {
-                    let jsonData = try JSONSerialization.data(withJSONObject: smartCollections)
-                    let decodedObject = try JSONDecoder().decode(T.self, from: jsonData)
-                    completion(decodedObject, nil)
-                } catch {
+            Alamofire.request(url).responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    guard let json = value as? [String: Any], let jsonObject = json[rootOfJson.rawValue] as? [[String: Any]] else {
+                        completion(nil, NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid JSON format"]))
+                        return
+                    }
+                    do {
+                        let jsonData = try JSONSerialization.data(withJSONObject: jsonObject)
+                        completion(jsonData, nil)
+                    } catch {
+                        completion(nil, error)
+                    }
+                case .failure(let error):
                     completion(nil, error)
                 }
-            case .failure(let error):
-                completion(nil, error)
             }
         }
-    }
 }
