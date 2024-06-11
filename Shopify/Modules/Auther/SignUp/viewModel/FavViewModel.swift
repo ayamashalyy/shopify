c0895276d9562e976793442ffc60d964737b6f80
mayar <miarsaleh@gmail.com>
2024-06-11 16:52:49 -0700

    getFav

---
+++ Shopify/Modules/Auther/SignUp/viewModel/FavViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  FavViewModel.swift
+//  Shopify
+//
+//  Created by mayar on 11/06/2024.
+//
+
+import Foundation
+
+class FavViewModel {
+    
+    var bindResultToViewController : (()->()) = {}
+    
+    var LineItems : [LineItem]?{
+        didSet{
+            bindResultToViewController()
+            print("LineItems     is    \(LineItems?.count)")
+        }
+    }
+    
+    func getFavs() {
+       // let addition = "\(Authorize.favDraftOrder()!).json"
+     
+        // for testin the get and show but real should be the above
+        
+       let addition = "1184814104824.json"
+        print( "additon in fav Authorize.favDraftOrder \(addition)")
+
+        NetworkManager.fetchDataFromApi(endpoint: .specficDraftOeder, rootOfJson: .specificDraftOrder, addition: addition) { data , error in
+            guard let data = data, error == nil else {
+                print("error in data")
+                return
+            }
+            
+            Decoding.decodeData(data: data, objectType: DraftOrder.self) { [weak self] (draftOrder, decodeError) in
+                guard let self = self else { return }
+                if let draftOrder = draftOrder {
+                    self.LineItems = draftOrder.line_items
+
+                }
+            }
+        }
+    }
+
+    
+    
+    
+}
--- Shopify/Modules/Fav/view/AllFavViewController.swift
+++ Shopify/Modules/Fav/view/AllFavViewController.swift
@@ -7,14 +7,22 @@
 
 import UIKit
 
+
+struct FavLineItem {
+    let name: String
+    let productId : Int
+    let image : String
+    let price : String
+}
+
 class AllFavViewController: UIViewController {
     
+    var favViewModel : FavViewModel?
+    let indicator = UIActivityIndicatorView(style: .large)
+    var myfavLineItem : [FavLineItem] = []
+
+    
     @IBOutlet weak var allFavTable: UITableView!
-    
-    var favImgs = ["m", "a", "s"]
-    var favsNames = ["product1", "product2", "product3"]
-    var favPrices = ["10 $", "20 $", "30 $"]
-    
     @IBAction func back(_ sender: Any) {
         dismiss(animated: true)
     }
@@ -23,22 +31,54 @@ class AllFavViewController: UIViewController {
         super.viewDidLoad()
         allFavTable.dataSource = self
         allFavTable.delegate = self
+        
         allFavTable.register(UINib(nibName: "WishListViewCell", bundle: nil), forCellReuseIdentifier: "WishListViewCell")
         allFavTable.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         
+        favViewModel = FavViewModel()
         checkIfNoData()
+        setUpUI()
+        
+        favViewModel?.bindResultToViewController = { [weak self] in
+            DispatchQueue.main.async {
+                guard let self = self, let lineItems = self.favViewModel?.LineItems else { return }
+                
+                for lineItem in lineItems {
+                    if let productId = lineItem.product_id,
+                       let imageUrl = lineItem.properties?.first(where: { $0.name == "imageUrl" })?.value,
+                       let price = lineItem.price {
+                        let favLineItem = FavLineItem(name: lineItem.title ?? "", productId: lineItem.product_id!, image: imageUrl, price: price)
+                        self.myfavLineItem.append(favLineItem)
+                        print("Fav: \(favLineItem)")
+                    } else {
+                        print("Error: Missing data in lineItem \(lineItem)")
+                    }
+                }
+                self.allFavTable.reloadData()
+                self.indicator.stopAnimating()
+            }
+        }
+
+        favViewModel?.getFavs()
     }
     
+    
     func checkIfNoData() {
-        if favsNames.isEmpty {
+        if myfavLineItem.isEmpty {
             allFavTable.backgroundView = createNoDataBackgroundView()
         } else {
             allFavTable.backgroundView = nil
         }
     }
     
+    
+    func setUpUI() {
+        indicator.center = self.view.center
+        indicator.startAnimating()
+        view.addSubview(indicator)
+    }
+    
     func createNoDataBackgroundView() -> UIView {
-        // Create image view
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "heart")?.withTintColor(.orange, renderingMode: .alwaysOriginal)
         imageView.contentMode = .scaleAspectFit
@@ -46,7 +86,6 @@ class AllFavViewController: UIViewController {
         imageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
-        // Create label
         let label = UILabel()
         label.text = "No Favorites yet. Add to it"
         label.textColor = .orange
@@ -54,18 +93,15 @@ class AllFavViewController: UIViewController {
         label.font = UIFont.systemFont(ofSize: 16)
         label.translatesAutoresizingMaskIntoConstraints = false
         
-        // Create stack view
         let stackView = UIStackView(arrangedSubviews: [imageView, label])
         stackView.axis = .vertical
         stackView.alignment = .center
         stackView.spacing = 10
         stackView.translatesAutoresizingMaskIntoConstraints = false
         
-        // Create container view
         let containerView = UIView()
         containerView.addSubview(stackView)
         
-        // Center stack view in container view
         NSLayoutConstraint.activate([
             stackView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
@@ -76,31 +112,35 @@ class AllFavViewController: UIViewController {
 }
 
 extension AllFavViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let count = favsNames.count
-        if count == 0 {
-            tableView.backgroundView = createNoDataBackgroundView()
-        } else {
-            tableView.backgroundView = nil
+           func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            let count = myfavLineItem.count
+            if count == 0 {
+                tableView.backgroundView = createNoDataBackgroundView()
+            } else {
+                tableView.backgroundView = nil
+            }
+            return count
         }
-        return count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "WishListViewCell", for: indexPath) as! WishListViewCell
-        print("WishListViewCell")
-        cell.productName.text = favsNames[indexPath.row]
-        cell.productPrice.text = favPrices[indexPath.row]
-        cell.favImage.image = UIImage(named: favImgs[indexPath.row])
-        return cell
-    }
-    
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "WishListViewCell", for: indexPath) as! WishListViewCell
+            let favItem = myfavLineItem[indexPath.row]
+            cell.productName.text = favItem.name
+            cell.productPrice.text = favItem.price
+            print("URL(string: favItem.image)\(URL(string: favItem.image))")
+            cell.favImage.kf.setImage(with: URL(string: favItem.image))
+
+            return cell
+        }
+        
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        Navigation.ToProduct(productId: "8575847989496", from: self)
+        var currentProduct = myfavLineItem[indexPath.row]
+        Navigation.ToProduct(productId: String(currentProduct.productId), from: self)
     }
     
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
@@ -110,9 +150,7 @@ extension AllFavViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Remove the item from your data source (e.g., API or Realm)
-            favsNames.remove(at: indexPath.row)
-            favPrices.remove(at: indexPath.row)
-            favImgs.remove(at: indexPath.row)
+            myfavLineItem.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
             checkIfNoData()
         }
